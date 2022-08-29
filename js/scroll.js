ff4a748956a7aa36d2dbbe5a9378b04413173dfd
gafleury <gabrielfleury95@gmail.com>
2022-08-28 22:31:54 -0300

    remove scroll

--- js/scroll.js
+++ js/scroll.js
@@ -4,25 +4,25 @@ const pageContainer = document.querySelector("[data-scroll-container]");
 
 const scroller = new LocomotiveScroll({
     el: pageContainer,
-    smooth: true,
+    smooth: false,
     mobile: {
         breakpoint: 0,
-        smooth: true
+        smooth: false
     },
     tablet: {
         breakpoint: 0,
-        smooth: true
+        smooth: false
     }
 });
 
 
 scroller.on("scroll", (instance) => {
     let height = window.innerHeight;
-    let customCursor = document.querySelector(".cursor");
+    // let customCursor = document.querySelector(".cursor");
     let footer = document.querySelector(".footer");
     let chat = document.querySelector(".chat");
     let scrollPy = instance.scroll.y + "px";
-    customCursor.style.top = scrollPy;
+    // customCursor.style.top = scrollPy;
     footer.style.transform = "translateY(" + scrollPy + ")";
     chat.style.top = instance.scroll.y + (height*.85) + "px";
     chat.style.opacity = 0 + (instance.scroll.y * 0.005);
